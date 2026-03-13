7316ddb0da57d1941536caebb1435f8efb742f8b
Farjana Akter Mim <farjanaaktermim330@gmail.com>
2026-03-14 00:37:26 +0600

    added user search feature

--- src/pages/Dashboard/UsersManagement/UsersManagement.jsx
+++ src/pages/Dashboard/UsersManagement/UsersManagement.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import useAxiosSecure from '../../../hooks/useAxiosSecure'
 import { useQuery } from '@tanstack/react-query';
 import { FiShieldOff } from "react-icons/fi";
@@ -8,11 +8,13 @@ import Swal from 'sweetalert2';
 const UsersManagement = () => {
 
   const axiosSecure = useAxiosSecure();
+    const [searchText, setSearchText] = useState('')
+
 
   const { data: users = [], refetch } = useQuery({
-    queryKey: ['users'],
+    queryKey: ['users', searchText],
     queryFn: async () => {
-      const res = await axiosSecure.get('/users');
+      const res = await axiosSecure.get(`/users?searchText=${searchText}`);
       return res.data;
     }
   })
@@ -166,6 +168,26 @@ const UsersManagement = () => {
       <h2 className="text-4xl mb-6">
         Manage Users: {users.length}
       </h2>
+
+      <label className="input">
+  <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <g
+      strokeLinejoin="round"
+      strokeLinecap="round"
+      strokeWidth="2.5"
+      fill="none"
+      stroke="currentColor"
+    >
+      <circle cx="11" cy="11" r="8"></circle>
+      <path d="m21 21-4.3-4.3"></path>
+    </g>
+  </svg>
+  <input 
+  onChange={(e) => setSearchText(e.target.value)}
+  type="search" 
+  className="grow" 
+  placeholder="Search Users" />
+</label>
 
       <div className="overflow-x-auto">
 
